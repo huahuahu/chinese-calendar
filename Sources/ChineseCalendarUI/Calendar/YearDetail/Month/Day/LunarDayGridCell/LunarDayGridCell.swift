d1ef199bc17b847fb3d6a635a2948f550e1ae357
tiger <guotenghu@gmail.com>
2026-09-22 21:40:54 +0800

    style: format lunar day cell border modifiers

--- Sources/ChineseCalendarUI/Calendar/YearDetail/Month/Day/LunarDayGridCell/LunarDayGridCell.swift
+++ Sources/ChineseCalendarUI/Calendar/YearDetail/Month/Day/LunarDayGridCell/LunarDayGridCell.swift
@@ -4,7 +4,7 @@ import SwiftUI
 
 /// 显示在 LunarMonthGrid 的日期网格中，用于呈现单个农历日。
 struct LunarDayGridCell: View {
-    private struct Constants {
+    private enum Constants {
         static let spacing: CGFloat = 6
         static let titleHorizontalPadding: CGFloat = 6
         static let titleVerticalPadding: CGFloat = 2
